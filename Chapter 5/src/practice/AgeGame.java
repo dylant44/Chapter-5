6808004c04328bd24b2a0bdd03b214ed61f06f00
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-18 09:53:37 -0500

    if else

--- Chapter 5/src/practice/AgeGame.java
+++ Chapter 5/src/practice/AgeGame.java
@@ -15,37 +15,37 @@ public class AgeGame
 		usersAge = input.nextInt();
 		
 		if(usersAge <= 12)
-		{
-			result = "You are a child! Go away!";
-		}
+			{
+				result = "You are a child! Go away!";
+			}
 		else if(usersAge >= 13  && usersAge <= 18)
-		{
+			{
 			result = "You are a snotty teen";
-		}
+			}
 		else if(usersAge >= 19 && usersAge <= 25)
-		{
-			result = "Get a job!";
-		}
+			{
+				result = "Get a job!";
+			}
 		else if(usersAge >= 26 && usersAge <= 30)
-		{
-			result = "You're old";
-		}
+			{
+				result = "You're old";
+			}
 		else if(usersAge >= 31 && usersAge <= 40)
-		{
-			result = "You're really old";
-		}
+			{
+				result = "You're really old";
+			}
 		else if(usersAge >= 41 && usersAge <= 50)
-		{
-			result = "You're super old";
-		}
+			{
+				result = "You're super old";
+			}
 		else if(usersAge >= 51 && usersAge <= 60)
-		{
-			result = "You're ancient";
-		}
+			{
+				result = "You're ancient";
+			}
 		else if(usersAge >= 61)
-		{
-			result = "You're probably gonna die soon";
-		}
+			{
+				result = "You're probably gonna die soon";
+			}
 		
 		
 		
