28b33326d4ffcf80ff6e29d352d0f559848ae41e
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-25 09:03:49 -0500

    condo sales

--- Chapter 5/src/exercises/CondoSales.java
+++ Chapter 5/src/exercises/CondoSales.java
@@ -7,9 +7,11 @@ public class CondoSales
 
 	public static void main(String[] args) 
 	{
-		String result;
+		String result = null;
 		int condoChoice;
-		
+		int parkingType;
+		int fullPrice;
+		int garagePrice = 5000;
 		Scanner input = new Scanner(System.in);
 		do {
 		System.out.print("Pick a condo, press 1 for a park view, 2 for a golf course view, or 3 for a lake view\n"
@@ -19,20 +21,58 @@ public class CondoSales
 		
 		if(condoChoice == 1)
 		{
-			result = "Park view condos are $150,000";
+			int condoPrice = 150000;
+			System.out.print("1 for garage, 2 for parking spot");
+			parkingType = input.nextInt();
+			if(parkingType == 1)
+			{
+				fullPrice = condoPrice + garagePrice;
+				result = "Park view condos with a garage are $" + fullPrice;
+			}
+			else if(parkingType == 2)
+			{
+				result = "Park view condos with a parking spot are $" + condoPrice;
+			}
+			
 		}
 		else if(condoChoice == 2)
 		{
-			result = "Golf course view condo's are $170,000";
+			int condoPrice = 170000;
+			System.out.print("1 for garage, 2 for parking spot");
+			parkingType = input.nextInt();
+			if(parkingType == 1)
+			{
+				fullPrice = condoPrice + garagePrice;
+				result = "Golf course view condos with a garage are $" + fullPrice;
+			}
+			else if(parkingType == 2)
+			{
+			result = "Golf course view condo's are $" + condoPrice;
+			}
 		}
 		else if(condoChoice == 3)
 		{
-			result = "Lake view condos are $210,000";
+			int condoPrice = 210000;
+			System.out.print("1 for garage, 2 for parking spot");
+			parkingType = input.nextInt();
+			if(parkingType == 1)
+			{
+				fullPrice = condoPrice + garagePrice;
+				result = "Lake view condos with a garage are $" + fullPrice;
+			}
+			else if(parkingType == 2)
+			{
+			result = "Lake view condos are $" + condoPrice;
+			}
 		}
 		else 
 		{
 			result = "That is not a choice";
 		}
+		
+		
+		
+		
 		displayMessage(result);
 		}while(condoChoice != 999);
 		
