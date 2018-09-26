d30446d0f466c43baf6be90d329302575b9626a3
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-26 09:07:03 -0500

    pay exercise

--- Chapter 5/src/exercises/Pay.java
+++ Chapter 5/src/exercises/Pay.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 public class Pay {
 
 	public static void main(String[] args) {
-		double skill;
+		int skill;
 		double hours;
 		double insurance;
 		double retirement = 0;
 		double rate = 0;
 		double grossPay;
-		double retirePercent;
+		double regPay = 0;
+		double retirePercent = 0;
 		double netPay;
-		double insurOption;
-		double overtPay;
-		double finalPay;
+		double insurOption = 0;
+		double overtimePay = 0;
+		double finalPay = 0;
+		double insurMoney = 0;
 		
 		Scanner input = new Scanner(System.in);
 		System.out.print("What is your skill level?");
-		skill = input.nextDouble();
+		skill = input.nextInt();
 		System.out.print("How many hours did you work?");
 		hours = input.nextDouble();
 		if(skill >= 1)
@@ -46,14 +48,54 @@ public class Pay {
 		{
 			rate = 22;
 		}
-		grossPay = hours * rate;
-		
+		if(hours <= 40)
+		{
+		regPay = hours * rate;
+		overtimePay = 0;
+		}
+		if(hours >= 40)
+		{
+		regPay = 40 * rate;
+		overtimePay = (hours - 40) * (rate * 1.5);
+		}
+		grossPay = regPay + overtimePay;
 		if(retirement == 1)
 		{
 		retirePercent = (3 * grossPay) / 100;	
 		}
+		else
+		{
+			retirePercent = 0;
+		}
+		netPay = grossPay - retirePercent;
 		
-		
+		if(skill >= 1)
+		{
+			if(insurOption == 1)
+			{
+				insurMoney = 32.5;
+			}
+			else if(insurOption != 1)
+			{
+				insurMoney = 20;
+			}
+			else if(insurOption == 3)
+			{
+				insurMoney = 10;
+			}
+			
+		}
+		finalPay = netPay - insurMoney;
+		if(finalPay <= 0)
+		{
+			System.out.print("Error, deductions exceed gross pay");
+		}
+		else
+		{
+			System.out.print("Hours: " + hours + " Rate: " + rate + " Regular pay: " + regPay 
+					+ " Overtime pay: "+ overtimePay + " Total pay:" + grossPay + " Total deductions " +
+					(retirePercent + insurMoney) + " Net pay: " + finalPay);
+		}
 		
 	}
 
